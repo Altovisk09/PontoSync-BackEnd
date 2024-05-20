df1e3fb301d99bcec54aed297075254c0d6ecfcb
Altovisk09 <ericaltao13@gmail.com>
2024-05-20 20:33:42 -0300

    Conexão com o banco estabelecida, cadastro funcionando

--- src/config/firebase/config.js
+++ src/config/firebase/config.js
@@ -1,32 +1,18 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
+const admin = require("firebase-admin");
+const { getFirestore } = require('firebase-admin/firestore');
 const { config } = require("dotenv");
 
 config();
 
-const firebaseConfig = {
-    type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    universe_domain: "googleapis.com"
-};
-
 const initializeFirebase = () => {
     try {
-        const firebaseApp = initializeApp({
-            credential: cert(firebaseConfig),
-            projectId: process.env.FIREBASE_PROJECT_ID
+        const serviceAccount = require("./pontoSync.json");
+        const firebaseApp = admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
         });
-        const firestore = getFirestore(firebaseApp);
+        const db = getFirestore(firebaseApp);
         console.log("Firebase initialized successfully");
-        return firestore;
+        return db;
     } catch (error) {
         console.error("Error initializing Firebase:", error);
         throw error;
--- src/models/Users.js
+++ src/models/Users.js
@@ -23,12 +23,32 @@ class Users {
                 disabled: false
             });
             const userId = userRecord.uid;
-
+            
+            await this.addToFireStore(userId);
+            
             console.log('Usuário criado com sucesso!');
-            return userId; // Retorne o ID do usuário criado
+            return; // Retorne o ID do usuário criado
         } catch (error) {
             console.error('Erro ao criar usuário:', error);
             throw error;
+        }
+    }
+    async addToFireStore(userId) {
+        try{
+            const db = getFirestore();
+        const userRef = db.collection("users").doc(userId);
+
+        await userRef.set({
+            name: this.name,
+            last_name: this.last_name,
+            phone_number: this.phone_number,
+            security_question: this.security_question,
+            response: this.response
+        })
+
+        console.log('Informações do usuario adicionadas ao Firestore!')
+        }catch(error){
+            console.error('Erro ao adicionar informações do usuário ao Firestore:', error);
         }
     }
 }
