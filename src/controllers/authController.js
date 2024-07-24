70f8497f69b58cc4d31ded1e1f300906b1702fc9
Altovisk09 <ericaltao13@gmail.com>
2024-07-24 19:13:17 -0300

    correção cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,7 +22,7 @@ async function login(req, res) {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             maxAge: rememberMe ? 1000 * 60 * 60 * 24 * 30 : 1000 * 60 * 60,
-            sameSite: 'nome',
+            sameSite: 'none',
         });
 
         res.status(200).json({ userData });
