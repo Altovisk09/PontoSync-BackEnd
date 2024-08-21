61e75539950d9c4eb8200b36090dcac3ed065aa8
Altovisk09 <ericaltao13@gmail.com>
2024-08-21 18:00:21 -0300

    alteração firebase

--- src/config/firebase/config.js
+++ src/config/firebase/config.js
@@ -3,18 +3,21 @@ const { getFirestore } = require('firebase-admin/firestore');
 const { config } = require("dotenv");
 
 config();
+let firebaseApp;
 
 const initializeFirebase = () => {
     try {
-        const serviceAccount = {
-            "project_id": process.env.GOOGLE_PROJECT_ID,
-            "client_email": process.env.GOOGLE_CLIENT_EMAIL,
-            "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          };
-
-        const firebaseApp = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-        });
+        if(!admin.apps.length){
+            const serviceAccount = {
+                "project_id": process.env.GOOGLE_PROJECT_ID,
+                "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+                "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            };
+            firebaseApp = admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+            });
+        }
+             
         const db = getFirestore(firebaseApp);
         console.log("Firebase initialized successfully");
         return db;
